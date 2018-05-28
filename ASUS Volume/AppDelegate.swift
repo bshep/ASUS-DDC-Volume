5d2d323105e5a7106b88a6db467337e0fdb38b80
Bruce Sheplan <git.bshep@xoxy.net>
2018-05-27 21:27:36 -0500

    Initial Commit

---
+++ ASUS Volume/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ASUS Volume
+//
+//  Created by Bruce Sheplan on 5/27/18.
+//  Copyright © 2018 Bruce Sheplan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ ASUS Volume/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ASUS Volume
+//
+//  Created by Bruce Sheplan on 5/27/18.
+//  Copyright © 2018 Bruce Sheplan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
