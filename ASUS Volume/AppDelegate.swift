21775373d04ac9781631b55d53b08ce63ac3bd83
Bruce Sheplan <git.bshep@xoxy.net>
2018-06-08 12:00:40 -0500

    - Bug fix: Slider would not load the save volume level - Added submenu to display list of moniors

--- ASUS Volume/AppDelegate.swift
+++ ASUS Volume/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	@IBOutlet weak var statusSlider: NSSlider!
 	@IBOutlet weak var volumeMenuItem: NSMenuItem!
 	@IBOutlet weak var beepMenuItem: NSMenuItem!
+	@IBOutlet weak var monitorsSubMenu: NSMenu!
 	
 	var currentVolume: Int32 = 0
 	var beepEnable: String = "on"
@@ -45,6 +46,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		}
 		
 		volumeMenuItem.title = "Volume: \(currentVolume)"
+		statusSlider.intValue = currentVolume
 		updateVolume(1, currentVolume)
 		
 		
@@ -101,7 +103,42 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		
 	}
 	
+	@IBAction func menuMonitorsAction(_ sender: NSMenuItem) {
+//
+//		for menu in sender.parent.item
+//
+//		for screen in NSScreen.screens {
+//			let screenNumber = description.object(forKey: "NSScreenNumber")  as! Int
+//
+//			if sender.tag == screenNumber {
+//				print "found screen"
+//			}
+//		}
+//
+//
+	}
+	
+	
 	func constructMenu() {
+		var newMenu : NSMenuItem
+		
+		for screen in NSScreen.screens {
+			newMenu = NSMenuItem()
+			
+			let description = screen.deviceDescription as NSDictionary
+			let resolution = description.object(forKey: NSDeviceDescriptionKey.size) as! NSSize
+			let screenNumber = description.object(forKey: "NSScreenNumber")  as! Int
+			
+			let resolutionString = "Screen - \(Int32(resolution.width))x\(Int32(resolution.height))"
+			
+			newMenu.title = resolutionString
+			newMenu.action = #selector(menuMonitorsAction(_:))
+			newMenu.tag = screenNumber
+			
+			monitorsSubMenu.addItem(newMenu)
+			
+		}
+		
 		statusSlider.setFrameSize(NSSize(width: 60, height: 100))
 		sliderMenuItem.view = statusSlider
 	
